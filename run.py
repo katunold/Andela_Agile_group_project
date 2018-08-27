f84320c537ecec016f8dfeb02e73b55d9092becc
ARTY-PC\Arty <arthurnangai@live.com>
2018-08-27 11:01:49 +0300

    basic functionality added. Changes to be committed: 	new file:   run.py

---
+++ run.py
@@ -0,0 +1,11 @@
+print("Please login")
+email = input()
+print(email)
+print("input password")
+password = input()
+
+print("Select an option \n 1. enter sale")
+option_selected = input()
+if option_selected == '1':
+    print("Enter item code")
+    
