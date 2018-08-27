058d0c49aeb0691284b3c2b2ed806f084a22567e
ARTY-PC\Arty <arthurnangai@live.com>
2018-08-27 12:51:03 +0300

    signup and login functionality added to system. Changes to be committed: 	modified:   run.py 	modified:   user.py

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from sale import Sales
-
+from user import User, add_user, login
 def make_sale():
     print("Enter item code")
     item_code = input()
@@ -9,16 +9,25 @@ def make_sale():
     item_price = 4
     new_sale = Sales(item_code, item_price, quantity_sold, 1)
     new_sale.print_receipt()
+    #update stock
 
 def check_stock():
     print("Enter stock id")
     stock_id = input()
-    
-print("Please login")
-email = input()
-print(email)
-print("input password")
-password = input()
+    #run check stock function
+
+def user_signup():
+    print("Enter email to signup")
+    email = input()
+    print("Enter admin role")
+    admin_role = input()
+    print("Enter password")
+    password = input()
+    new_user = User(email, admin_role, password)
+    print(add_user(new_user))
+
+
+user_signup()
 
 selection_menu ="Select an option \n 1. enter sale" 
 selection_menu += "\n 2. check stock"
--- user.py
+++ user.py
@@ -1,4 +1,19 @@
 from database import Database
+users = {}
 
-class User(Database):
-    pass
+class User():
+    
+    def __init__(self, email, admin_role, password):
+        self.email = email
+        self.admin_role = admin_role
+        self.password = password
+
+def add_user(new_user):
+    no_of_users = len(users) + 1
+    users[no_of_users] = new_user.__dict__
+    return users[no_of_users]
+
+def login(login_id, existing_user):
+    if login_id in users:
+        user = users[login_id]
+        return user['admin_role']
