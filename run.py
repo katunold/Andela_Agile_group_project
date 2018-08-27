bee847e4aa27f54d504a02702bf0dfd2401e94e1
ARTY-PC\Arty <arthurnangai@live.com>
2018-08-27 11:46:36 +0300

    check stock method added.

--- run.py
+++ run.py
@@ -10,14 +10,22 @@ def make_sale():
     new_sale = Sales(item_code, item_price, quantity_sold, 1)
     new_sale.print_receipt()
 
+def check_stock():
+    print("Enter stock id")
+    stock_id = input()
+    
 print("Please login")
 email = input()
 print(email)
 print("input password")
 password = input()
 
-print("Select an option \n 1. enter sale")
+selection_menu ="Select an option \n 1. enter sale" 
+selection_menu += "\n 2. check stock"
+print(selection_menu)
 option_selected = input()
 
 if option_selected == '1':
     make_sale()
+elif option_selected == '2':
+    check_stock()
